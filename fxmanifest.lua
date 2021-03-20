8ce2b50ca81033aafa779d79e7a220a1adeede7f
RealAndrewTheDev <79156045+RealAndrewTheDev@users.noreply.github.com>
2021-03-20 20:34:01 +0000

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+-- Resource Metadata
+fx_version 'bodacious'
+games { 'gta5' }
+
+author 'rubbertoe98'
+patchedtriggers 'ATD'
+description 'TakeHostage'
+version '1.0.0'
+
+client_script "cl_takehostage.lua"
+server_script "sv_takehostage.lua"
